e2e6e6ea5487bbdbfaf46959e9d3a734c13df455
VadimOlkhov <65563436+VadimOlkhov@users.noreply.github.com>
2021-02-11 13:37:55 +0300

    create manager

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,15 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(int authorID, String domain, String query, Boolean authorOnly, int count, int offset) {
+        return null;
+    }
+
+    public void delete(int authorId, int postId) {
+
+    }
+}
